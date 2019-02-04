eefedaf1958149b9e7d1178b897197de5b57086d
ConnectExtend <compass2k17@outlook.com>
2019-02-04 16:51:34 -0600

    activate button toggle

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -9,25 +9,42 @@ import { Manager, Reference, Popper } from 'react-popper';
 export default class TopSection extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isSelectLocationOpen: false
+        };
+    }
+
+    onToggleSelectLocation() {
+        this.setState(prevState => ({
+            isSelectLocationOpen: !prevState.isSelectLocationOpen
+        }));
     }
 
     render() {
+        const { isSelectLocationOpen } = this.state;
+        
         return (
             <div className='top-content'>
                 <div className='title'>Weather Express</div>
-                <Weather {...this.props} /><Manager>
+                <Weather {...this.props} />
+                <Manager>
                     <Reference>
-                    {({ ref }) => (
-                        <button className='btn-select-location'>Select Location</button>
-                    )}
+                        {({ ref }) => (
+                            <button className='btn-select-location' 
+                            ref={ref}
+                            onClick={this.onToggleSelectLocation.bind(this)}
+                            >
+                                Select Location
+                            </button>
+                        )}
                     </Reference>
                     <Popper placement="top">
-                        {({ ref, style, placement, arrowProps }) => (
+                        {({ ref, style, placement, arrowProps }) => ( 
+                            isSelectLocationOpen &&
                             <div ref={ref} style={style} data-placement={placement}>
                                 Popper element
                             <div ref={arrowProps.ref} style={arrowProps.style} />
-                        </div>
+                            </div>
                         )}
                     </Popper>
                 </Manager>
